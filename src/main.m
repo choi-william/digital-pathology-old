6ce6ddc4c07468ba612722ec6ed7efa95c21f928
choi1995 <w.choi@alumni.ubc.a>
2017-01-11 00:15:36 -0800

    Basic image thresholding

--- src/main.m
+++ src/main.m
@@ -1,3 +1,16 @@
 run('init.m');
 im = DPImage('A.tif');
-imshow(im.image);
+
+% converting image to grayscale
+grayIm = rgb2gray(im.image);
+A = size(grayIm);
+figure, imhist(grayIm);
+figure, imshow(grayIm);
+
+% T = adaptthresh(grayIm);
+% global thresholding Otsu's Method
+BW = imbinarize(grayIm);
+figure, imshow(BW);
+%thresIm = imregionalmax(I,26);
+%imshow(thresIm);
+
