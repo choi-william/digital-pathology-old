b4ead9db41180d01b7b03a21304b2951e46b5c28
akyriazis <adkyriazis@gmail.com>
2017-01-13 00:02:13 -0800

    implementation of mumford-shah algorithm

--- src/main.m
+++ src/main.m
@@ -1,16 +1,22 @@
+close all;
+
 run('init.m');
 im = DPImage('A.tif');
 
 % converting image to grayscale
 grayIm = rgb2gray(im.image);
-A = size(grayIm);
-figure, imhist(grayIm);
-figure, imshow(grayIm);
+%A = size(grayIm);
+%figure, imhist(grayIm);
+%figure, imshow(grayIm);
 
-% T = adaptthresh(grayIm);
+seg = prepare.mumford.chenvese(im.image,'whole',400,0.1,'multiphase'); % ability on gray image
+
+%T = adaptthresh(grayIm);
 % global thresholding Otsu's Method
-BW = imbinarize(grayIm);
-figure, imshow(BW);
+%BW = imbinarize(grayIm,60/255);
+%figure, imshow(BW);
 %thresIm = imregionalmax(I,26);
 %imshow(thresIm);
+
+%-- End 
 
