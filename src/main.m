21b5b7bf74d1daf621aeb872655a4c63b046957d
akyriazis <adkyriazis@gmail.com>
2017-02-05 14:12:32 -0800

    more refactoring

--- src/main.m
+++ src/main.m
@@ -11,9 +11,9 @@ E = DPImage('E.tif');
 [list,dp] = Prepare.extractSoma(A, 0);
 Display.displayOverlay(dp);
 
-%Verify.evaluate(prepare.extractSoma(A, 0));
-%Verify.evaluate(prepare.extractSoma(B, 0));
-%Verify.evaluate(prepare.extractSoma(C, 0));
-%Verify.evaluate(prepare.extractSoma(D, 0));
-%Verify.evaluate(prepare.extractSoma(E, 0));
+%Verify.evaluate_soma(prepare.extractSoma(A, 0));
+%Verify.evaluate_soma(prepare.extractSoma(B, 0));
+%Verify.evaluate_soma(prepare.extractSoma(C, 0));
+%Verify.evaluate_soma(prepare.extractSoma(D, 0));
+%Verify.evaluate_soma(prepare.extractSoma(E, 0));
 
