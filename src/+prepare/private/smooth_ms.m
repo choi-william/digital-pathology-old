bcf88caa09632bd60d9e61382c8cd0556f6bee36
choi1995 <wchoi@alumni.ubc.ca>
2017-03-05 18:04:36 -0800

    temporarily removing prepare

--- src/+prepare/private/smooth_ms.m
+++
@@ -1,8 +0,0 @@
-function [ out ] = smooth_ms( in, l, a, e)
-    if nargin == 4       
-        out = prepare.mumford_shah.fastms(in, 'lambda', l, 'alpha', a, 'edge', e);
-    elseif nargin == 3
-        out = prepare.mumford_shah.fastms(in, 'lambda', l, 'alpha', a);        
-    end
-end
-
